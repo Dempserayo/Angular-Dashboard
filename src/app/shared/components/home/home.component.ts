5a476fcfd8af313be5b38278611908e44f10b25b
RayeRayeRaye <novenacuenta709@gmail.com>
2025-05-08 23:23:39 -0500

    Feat: Footer - Navbar Component  + App component
    Component that will hold relevant information on the dashboard, yet to be modified.

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component, VERSION } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { FooterComponent } from "../footer/footer.component";
+import { CardComponent } from '../../../business/components/card/card.component';
+import { TablesComponent } from "../../../business/components/tables/tables.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, SidebarComponent],
+  imports: [NavbarComponent, SidebarComponent, FooterComponent, CardComponent, TablesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
