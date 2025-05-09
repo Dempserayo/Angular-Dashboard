a0ec1dc62e6d9a0371b37cbe34571d91d7ae54ef
RayeRayeRaye <novenacuenta709@gmail.com>
2025-05-09 17:36:37 -0500

    Feat: Card Component
    Advances in the development of the Card component, both in functionality and design.

--- src/app/business/components/card/card.component.ts
+++ src/app/business/components/card/card.component.ts
@@ -1,11 +1,14 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
-
+    totalCash: number = 40;
+    stateNumber: number = 20;
+    statePercentage: boolean = true;
 }
