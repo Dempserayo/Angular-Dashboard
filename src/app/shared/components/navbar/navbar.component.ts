5692ddf2bda55ee05ca8adbfa74f36d77783032d
RayeRayeRaye <novenacuenta709@gmail.com>
2025-05-09 16:42:37 -0500

    Feat: Sidebar
    Advances in the development of the Sidebar both in functionality and design

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "../sidebar/sidebar.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-navbar',
