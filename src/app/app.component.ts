375072650c7139b1a839e517ec4e717c4a86343d
RayeRayeRaye <novenacuenta709@gmail.com>
2025-05-10 16:51:36 -0500

    Feat: Angular Material Installation + Modification in Sidebar Component
    Installation of Angular Material, for the use of Icons in the project + Visual modifications in the Sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./shared/components/sidebar/sidebar.component";
-import { FooterComponent } from './shared/components/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent, FooterComponent],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
