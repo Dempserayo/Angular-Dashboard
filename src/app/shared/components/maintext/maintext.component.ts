e25ab050694ee0b8e85fb0b2d641e1de948458fb
RayeRayeRaye <novenacuenta709@gmail.com>
2025-05-11 22:16:01 -0500

    Feat: Creation of Main Text Component

---
+++ src/app/shared/components/maintext/maintext.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maintext',
+  imports: [],
+  templateUrl: './maintext.component.html',
+  styleUrl: './maintext.component.css'
+})
+export class MaintextComponent {
+
+}
--- src/app/shared/pages/home/home.component.ts
+++ src/app/shared/pages/home/home.component.ts
@@ -1,14 +1,14 @@
 import { Component, VERSION } from '@angular/core';
 
-import { TablesComponent } from "../../../business/components/tables/tables.component";
 import { CardComponent } from '../../../business/components/card/card.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { MaintextComponent } from "../../components/maintext/maintext.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, SidebarComponent, FooterComponent, CardComponent, TablesComponent],
+  imports: [NavbarComponent, SidebarComponent, FooterComponent, CardComponent, MaintextComponent, MaintextComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
