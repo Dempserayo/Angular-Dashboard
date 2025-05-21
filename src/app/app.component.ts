afd04a7d106f6d97cdad36bfc4aa5bc684f89f46
RayeRayeRaye <novenacuenta709@gmail.com>
2025-05-21 15:35:38 -0500

    Fix: Github Actions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./shared/components/sidebar/sidebar.component";
-import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +8,6 @@ import { trigger, transition, style, animate } from '@angular/animations';
   imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  animations: [
-    trigger('routeAnimations', [
-      transition('* <=> *', [
-        style({ opacity: 0, transform: 'translateY(1px)' }),
-        animate('0.5s ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
-      ])
-    ])
-  ]
 })
 export class AppComponent {
   title = 'angular-project';
